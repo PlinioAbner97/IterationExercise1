da6d4e9f8e43b756e7bc119dc2a9b7b8742927be
PlinioAbner <plinio414@gmail.com>
2022-03-23 14:01:03 -0400

    initial commit

---
+++ IrerationExercise1Console/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace IrerationExercise1Console
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+
+        //for (int i = 0; i< 5; i++)
+        //{
+        //Console.WriteLine($"{i}");
+        // }
+
+        // for(int i = 0; i< 5; i++)
+        // {
+        //Console.WriteLine($"{i}");
+        //  }
+
+        //for(int i = 0; i < 5; i++)
+        //{ 
+        // Console.WriteLine($"{i}");
+        // }
+
+        // for(int i = 0; i < 5; i++)
+        //  {
+        //Console.WriteLine($"{i}");
+        //  }
+
+        // for(int i = 0; i < 5; i++)
+        //  {
+        //  Console.WriteLine($"{i}");
+        // }
+
+        // for(int i = 0; i < 5; i++)
+        //  { 
+        // Console.WriteLine($"{i}");
+        // }
+
+        // for(int i = 0; i <5; i++)
+        //  { 
+        // Console.WriteLine($"{i}");
+        //  }
+
+        // for(int i = 0; i <5; i++)
+        // {    
+        // Console.WriteLine($"{i}");
+        // }
+
+        //  for(int i = 0; i < 5; i++)
+        // {
+        //  Console.WriteLine($"{i}");
+        // }
+
+        // for(int i = 0; i <5; i++)
+        // {
+        //  Console.WriteLine($"{i}");
+        //   }
+
+        //  for(int i = 0; i < 5; i++)
+        //   {
+        // Console.WriteLine($"{i}");
+        //  }
+
+        // for (int i = 0; i < 5; i++)
+        // {
+        // Console.WriteLine($"{i}");
+        //   }
+
+        //  for(int i = 0; i <5; i++)
+        // {
+        // Console.WriteLine($"{i}");
+        //  }
+
+        //  for(int i = 0; i < 5; i++)
+        //   {
+        //Console.WriteLine($"{i}");
+        //   }
+
+        //  for(int i = 0; i < 5; i++)
+        //  {
+        // Console.WriteLine($"{i}");
+        // }
+    }
+}
